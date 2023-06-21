b8faa526846a275ea448405249bfe3bb8ceda297
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2023-06-21 23:26:07 +0500

    Lesson 5: List rendering

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })
