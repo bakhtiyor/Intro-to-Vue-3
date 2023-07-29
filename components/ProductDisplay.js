d0bd600e6181ecde1c57080253732095a4471a0b
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2023-07-29 17:18:32 +0500

    Final changes

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -38,8 +38,9 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
-
       </div>
+      <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>
   </div>`,
   data() {
@@ -51,7 +52,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,6 +62,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   },
   computed: {
